f2bd2260d27669eba03deeeb34b3ea8912ea6b1a
debjyotiroy <debjyotiry@gmail.com>
2024-12-14 20:44:04 +0530

    added backend code template

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+//routes declare
+app.use("/api/v1/users", userRouter);
+export default app;
